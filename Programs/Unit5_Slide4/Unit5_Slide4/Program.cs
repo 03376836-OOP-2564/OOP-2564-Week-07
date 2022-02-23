89f205cd6ae85361a232bfad37b73c46b7a79ffb
Koson Trachu <koson.trachu@gmail.com>
2022-02-24 00:05:30 +0700

    add source code

---
+++ Programs/Unit5_Slide4/Unit5_Slide4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProgram // Declare a namespace.
+{
+    class DeclarationOfTypeA { };   // Declare a type.
+    struct DeclarationOfTypeB { };  // Declare a type.
+    class C                         // Declare a type.
+    {
+        static void Main() 
+        { 
+            
+        }
+    }
+}
